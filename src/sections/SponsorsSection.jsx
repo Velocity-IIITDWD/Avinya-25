6f4d890d4b9a9da0cc519e256beceade935ec285
Ujwal Akotkar <24bcs012@iiitdwd.ac.in>
2025-10-19 20:56:03 +0530

    Minor change
    Signed-off-by: Ujwal Akotkar <24bcs012@iiitdwd.ac.in>

--- src/sections/SponsorsSection.jsx
+++ src/sections/SponsorsSection.jsx
@@ -9,7 +9,7 @@ import ProfileCard from '../components/SponsorCard/SponsorCard.jsx'
 export default function SponsorsSection() {
   return (
     <>
-      <div className='mt-40 min-h-[60vh] sm:mt-30 m-auto px-8'>
+      <div className='mt-40 min-h-[60vh] sm:mt-30 sm:mb-60 m-auto px-8'>
         <h1 className='text-[4vw] text-white border-t-1 pt-5 border-white text-center mb-16 '>
           {/* <ShinyText text="SPONSORS" className="text-[7vw] text-white text-center mb-16" speed={3} /> */}
           <SponsorText text="SPONSORS" speed={3} />
