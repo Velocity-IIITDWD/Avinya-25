a40cf5a016fc1f7784b74765805270047408a6a1
Ujwal Akotkar <24bcs012@iiitdwd.ac.in>
2025-10-19 15:48:04 +0530

    Added hero section'
    Signed-off-by: Ujwal Akotkar <24bcs012@iiitdwd.ac.in>

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router'
+import Menu from './components/menu/Menu.jsx'
+
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
+    <Menu />
     <App />
-  </StrictMode>,
+  </BrowserRouter>
 )
