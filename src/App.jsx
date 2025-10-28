24a33a0ba0981aa50509990055632198165c5a31
kushagra2207 <fantastick22072006@gmail.com>
2025-10-28 17:04:17 +0530

    fix: scrolling on redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import './App.css'
 import { useLocomotiveScroll } from './hooks/useLocomotiveScroll.js'
 import Navbar from './components/Navbar/Navbar.jsx'
@@ -6,6 +6,7 @@ import HomePage from './pages/HomePage.jsx'
 import EventsPage from './pages/EventsPage.jsx'
 import ArtistsPage from './pages/ArtistsPage.jsx'
 import TeamPage from './pages/TeamPage.jsx'
+import { useEffect } from 'react'
 
 function App() {
   const { scrollRef, locomotiveRef } = useLocomotiveScroll();
@@ -13,6 +14,7 @@ function App() {
   return (
     <>
       <Router>
+        <ScrollToTop locomotiveRef={locomotiveRef} />
         <Navbar />
         <div ref={scrollRef} data-scroll-container>
           <div className='min-h-screen relative' data-scroll-section>
@@ -30,3 +32,26 @@ function App() {
 }
 
 export default App;
+
+function ScrollToTop({ locomotiveRef }) {
+  const location = useLocation();
+
+  useEffect(() => {
+    const handleScrollTop = () => {
+      if (locomotiveRef.current) {
+        locomotiveRef.current.scrollTo(0, { duration: 0, disableLerp: true });
+      } else {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      }
+    };
+
+    // On route change, scroll to top
+    handleScrollTop();
+
+    // Listen for explicit scroll-to-top requests
+    window.addEventListener('app-scroll-top', handleScrollTop);
+    return () => window.removeEventListener('app-scroll-top', handleScrollTop);
+  }, [location.pathname, locomotiveRef]);
+
+  return null;
+}
