d6d80a60625057a42fac16d9ea69b662eabbeacb
uju09 <24bcs012@iiitdwd.ac.in>
2025-10-31 12:43:24 +0530

    Added sponsor sec
    Signed-off-by: uju09 <24bcs012@iiitdwd.ac.in>

--- src/sections/PrevSponsorSec.jsx
+++ src/sections/PrevSponsorSec.jsx
@@ -5,7 +5,7 @@ import SponsorText from '../components/SponsorText/SponsorText.jsx'
 export default function SponsorsSection() {
   return (
     <>
-      <div className='mt-40 min-h-[60vh] sm:mt-30 sm:mb-40 m-auto px-8'>
+      <div className='mt-20 min-h-[60vh] sm:mt-30 sm:mb-40 m-auto px-8'>
         <h1 className='text-[4vw] text-white border-t-1 pt-5 border-white text-center mb-16 '>
           {/* <ShinyText text="SPONSORS" className="text-[7vw] text-white text-center mb-16" speed={3} /> */}
           <div className='w-full flex flex-col justify-between'>
