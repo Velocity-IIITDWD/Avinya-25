6573593d1783effd02376e853352b844c35599b5
kushagra2207 <fantastick22072006@gmail.com>
2025-11-05 02:32:10 +0530

    add member pic

--- src/data/team_data.js
+++ src/data/team_data.js
@@ -42,55 +42,55 @@ export const teamData = {
         {
             name: "Event Management - Technical",
             members: [
-                { name: "Vaasan", image: "/images/unknown.webp" },
                 { name: "Charvi", image: "/images/unknown.webp" },
-                { name: "Sadhgun", image: "/images/unknown.webp" }
+                { name: "Sadhgun", image: "/images/DASI SADHGUN RAM.webp" },
+                { name: "Vaasan", image: "/images/unknown.webp" },
             ]
         },
         {
             name: "SocialMedia",
             members: [
-                { name: "Varman", image: "/images/unknown.webp" },
+                { name: "Kuldeep", image: "/images/unknown.webp" },
                 { name: "Pothan", image: "/images/unknown.webp" },
                 { name: "Sohan", image: "/images/unknown.webp" },
-                { name: "Kuldeep", image: "/images/unknown.webp" }
+                { name: "Varman", image: "/images/unknown.webp" },
             ]
         }
     ],
     clubLeads: {
         cultural: [
+            { name: "Aadhya", image: "/images/AADHYA ROHISH.webp" },
             { name: "Bhoomika", image: "/images/BHOOMIKA MAHESH MANNUR.webp" },
+            { name: "Bikram", image: "/images/BIKRAM HAWLADAR.webp" },
             { name: "Hammad", image: "/images/HAMMAD MALIK.webp" },
-            { name: "Sree Samita", image: "/images/SREE SAMHITA GUNDIMEDA.webp" },
-            { name: "Om Pandey", image: "/images/OM MISHRA.webp" },
             { name: "Manoj", image: "/images/unknown.webp" },
+            { name: "Om Pandey", image: "/images/OM MISHRA.webp" },
             { name: "Saksham", image: "/images/unknown.webp" },
             { name: "Saurav", image: "/images/unknown.webp" },
-            { name: "Aadhya", image: "/images/AADHYA ROHISH.webp" },
-            { name: "Bikram", image: "/images/BIKRAM HAWLADAR.webp" },
+            { name: "Sree Samita", image: "/images/SREE SAMHITA GUNDIMEDA.webp" },
         ],
         technical: [
-            { name: "Sai satwik", image: "/images/unknown.webp" },
-            { name: "Amith Mathew", image: "/images/unknown.webp" },
             { name: "Abhinav", image: "/images/ABHINAV VERMA.webp" },
-            { name: "Aneesh", image: "/images/unknown.webp" },
-            { name: "Om Mishra", image: "/images/OM MISHRA.webp" },
-            { name: "Pradnesh", image: "/images/unknown.webp" },
-            { name: "Savya", image: "/images/unknown.webp" },
-            { name: "Simone", image: "/images/RODRIGUES SIMONE JOHN.webp" },
-            { name: "Shivambu", image: "/images/SHIVAMBU DEV PANDEY.webp" },
+            { name: "Amith Mathew", image: "/images/unknown.webp" },
             { name: "Anas", image: "/images/MOHD ANAS KHAN.webp" },
+            { name: "Aneesh", image: "/images/unknown.webp" },
             { name: "Arnav", image: "/images/unknown.webp" },
-            { name: "Shardendu", image: "/images/SHARDENDU MISHRA.webp" },
             { name: "Divyansh", image: "/images/DIVYANSH MISHRA.webp" },
             { name: "Ishan", image: "/images/ISHAN SRIVASTAVA.webp" },
             { name: "Kushagra", image: "/images/KUSHAGRA KUMAR ARORA.webp" },
+            { name: "Om Mishra", image: "/images/OM MISHRA.webp" },
+            { name: "Pradnesh", image: "/images/unknown.webp" },
+            { name: "Sai satwik", image: "/images/unknown.webp" },
+            { name: "Savya", image: "/images/unknown.webp" },
+            { name: "Shardendu", image: "/images/SHARDENDU MISHRA.webp" },
+            { name: "Shivambu", image: "/images/SHIVAMBU DEV PANDEY.webp" },
+            { name: "Simone", image: "/images/RODRIGUES SIMONE JOHN.webp" },
         ]
     }
     ,
     developers: [
-        { name: "Kushagra", image: "/images/KUSHAGRA KUMAR ARORA.webp" },
         { name: "Aditya", image: "/images/ADITYA GUPTA.webp" },
+        { name: "Kushagra", image: "/images/KUSHAGRA KUMAR ARORA.webp" },
         { name: "Ujwal", image: "/images/UJWAL AKOTKAR.webp" }
     ]
 };
