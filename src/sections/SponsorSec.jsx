6e19e92157a04892f12b03ffd23ee5cb1492ad9b
kushagra2207 <fantastick22072006@gmail.com>
2025-11-14 01:23:39 +0530

    add sponsors

--- src/sections/SponsorSec.jsx
+++ src/sections/SponsorSec.jsx
@@ -8,11 +8,25 @@ export default function SponsorsSection() {
                 <h1 className='text-[4vw] text-white border-t-1 pt-5 border-white text-center '>
                     <ShinyText text="SPONSORS" className="text-[7vw] text-white text-center" speed={3} />
                 </h1>
-                <div className='flex flex-wrap justify-center items-center'>
-                    <span className=' rounded-lg inline-block p-10'>
-                        <img className='w-[20vw] sm:w-[15vw] m-auto' src="/icons/sbi2.png" alt="" />
-                        <ShinyText text="State Bank of India" className="text-[3vw] text-white text-center mt-3" speed={3} />
-                    </span>
+                <div className='flex flex-col md:flex-row justify-between items-center gap-10 mt-10 md:px-10'>
+                    {[
+                        { img: '/icons/sbi2.png', name: 'State Bank of India' },
+                        { img: '/icons/Canara.png', name: 'Canara Bank' },
+                        { img: '/icons/chocoberry.jpg', name: '#007 Chocoberry' },
+                    ].map((s, i) => (
+                        <div key={i} className='flex flex-col items-center justify-center w-[200px] sm:w-[250px] h-[250px] bg-transparent'>
+                            <img
+                                src={s.img}
+                                alt={s.name}
+                                className={`object-contain m-auto ${i === 1 ? 'w-[200px] h-[200px]' : 'w-[140px] h-[140px]'}`}
+                            />
+                            <ShinyText
+                                text={s.name}
+                                className="text-[6vw] sm:text-[2.5vw] text-white text-center mt-5"
+                                speed={3}
+                            />
+                        </div>
+                    ))}
                 </div>
             </div >
         </>
